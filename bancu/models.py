79b6e4cc6b521405736a3894c05435cb8778cfde
Ewerton Oliveira <ewerton.amorim@live.com>
2018-01-12 10:29:12 -0300

    Criado CLasses Usuario, Agencia e Cartao

--- bancu/models.py
+++ bancu/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 
 # Usuário
+class Usuario(models.Model):
+    nome = models.CharField()
+    endereco = models.CharField()
+    cpf = models.IntegerField(primary_key=True)
 
 # Conta Usuário
 
 # Agência
-
+class Agencia(models.Model):
+    pass
 # Tipo Conta
 
 # Conta Corrente e Poupança
 
 # Cartão
-
+class Cartao(models.Model):
+    numero = models.IntegerField()
+    conta = models.ForeignKey(ContaUsuario)
+    validade = models.CharField()
+    codigo_seg = models.IntegerField()
+    nome_abv = models.CharField()
 
