710b8f17b95fed48ef879e875156dbccdf554f9f
cayolopes <cayolopesbc@gmail.com>
2018-01-12 11:30:35 -0200

    TipoConta

--- bancu/models.py
+++ bancu/models.py
@@ -7,6 +7,12 @@ from django.db import models
 # Agência
 
 # Tipo Conta
+class TipoConta(models.Model):
+    TIPO_CONTA = ((1,'Conta Poupança'),(2,'Conta Corrente'))
+
+    tipo = models.CharField(max_lenght = 10, choices = TIPO_CONTA, blank = True)
+
+
 
 # Conta Corrente e Poupança
 
