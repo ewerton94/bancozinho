c338fe141fbaeec1b26585419f79a3b3af1aa398
Ewerton Oliveira <ewerton.amorim@live.com>
2018-01-12 13:59:40 -0300

    rrgrgr

--- bancozinho/urls.py
+++ bancozinho/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from bancu.views import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index$', hello)
+
 ]
